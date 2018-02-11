822c87f8b18ec84abfc7b787eae6c13ddcbdc8e2
Kodam, Sheshank <Sheshank.Kodam@echostar.com>
2018-02-11 11:41:27 -0700

    - Added database info -

---
+++ database.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+
+database_port = 27017
+database_collection = "social_fitness"
+
+client = MongoClient(port=database_port)
+
+db_host = "mongodb://localhost:27017/"
+db_name = "social_fitness"
+db_client = MongoClient(port=database_port)
+db = db_client[db_name]
+db_collection = db["iot"]
+
+
+def insert(data):
+    # Step 3: Insert business object directly into MongoDB via isnert_one
+    db_collection.insert_one(data)
+    return
