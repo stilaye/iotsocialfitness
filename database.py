bd172b2f8459201e8dabc8b3cdcc5da8ccf7afbc
Kodam, Sheshank <Sheshank.Kodam@echostar.com>
2018-02-11 13:54:09 -0700

    - USed different collection types for coffee mahines and pots

--- database.py
+++ database.py
@@ -9,30 +9,47 @@ db_host = "mongodb://localhost:27017/"
 db_name = "social_fitness"
 db_client = MongoClient(port=database_port)
 db = db_client[db_name]
-db_collection = db["iot"]
+machine_coll = db["machine"]
+users_coll = db["users"]
+
+_COFFE_POT = ["coffee_pot", "coffee pot", "coffee machine", "coffee_machine"]
 
 
 def update_sensor_data(data):
-    db_collection.update(
-        {
-            "user": str(data['user'])
-        },
-        {
-            "$set": {
-                "device_status": data.get('device_status'),
-                "device_id": data.get("device_id"),
-                "type": data.get("type")
-            }
-        }, upsert=True)
+    device_type = data.get("type")
+    if device_type in _COFFE_POT:
+        machine_coll.update(
+            {
+                "user": str(data['user'])
+            },
+            {
+                "$set": {
+                    "device_status": data.get('device_status'),
+                    "device_id": data.get("device_id"),
+                    "type": data.get("type")
+                }
+            }, upsert=True)
+    else:
+        users_coll.update(
+            {
+                "user": str(data['user'])
+            },
+            {
+                "$set": {
+                    "device_status": data.get('device_status'),
+                    "device_id": data.get("device_id"),
+                    "type": data.get("type")
+                }
+            }, upsert=True)
 
 
 def update_user_data(data):
-    db_collection.update(
+    users_coll.update(
         {
             "user": str(data['user'])
         },
         {
-            "$set": {
+            "$setOnInsert": {
                 "phone": data.get('phone'),
                 "email": data.get("email"),
                 "mac_address": data.get("mac_address"),
