119dbe62c9831aac8bc214ee4b540edd3f89bb23
Tilaye, Swapnil <Swapnil.Tilaye@echostar.com>
2018-02-11 10:32:43 -0700

    initial commit

---
+++ socialfiot.py
@@ -0,0 +1,24 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/status')
+def status():
+    return "Social Fitness IoT"
+
+@app.route('/api/v1/social')
+def social():
+    return "Hello World!"
+
+@app.route('/api/v1/user/data')
+def user_data():
+    return "Hello World!"
+
+@app.route('/api/v1/sensor/data')
+def sensor_data():
+    return "Hello World!"    
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=2001)
+
+
+   
